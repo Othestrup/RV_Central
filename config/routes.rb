f26a0eefdc40fcdba21719b75b902cfff751b3ed
Olle Thestrup <thestrup@unlv.nevada.edu>
2021-11-16 15:55:06 +0100

    index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :users, only: [:index, :show, :create, :new]
 
-  resources :campers, only: [:create, :new, :update, :edit, :destroy] do
+  resources :campers, only: [:index, :create, :new, :update, :edit, :destroy] do
     resources :bookings, only: [:create, :new, :edit, :update, :destroy]
   end
 end
