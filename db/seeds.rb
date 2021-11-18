31123798724f7a6e7eccc1570ed3bf60fdf4904c
albert auge <aaugeserrano@gmail.com>
2021-11-18 14:17:53 +0100

    fixes in the style of all the website and the creation of pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,11 @@ require 'faker'
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Booking.destroy_all
+
+Camper.destroy_all
+
+User.destroy_all
 
 user = User.create!(email: "user@user.com", password: "password")
 
